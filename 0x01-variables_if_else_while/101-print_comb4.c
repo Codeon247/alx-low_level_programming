c927a1d2550c44feb51476eb60c041089032bed0
Codeon247 <ugwuokefrank02@gmail.com>
2022-09-09 17:17:22 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,35 @@
 nclude <stdio.h>
-
-
 /**
- *  * main -  prints all possible different combinations of three digits.
- *   *
- *    * Description: your code should be in the main function
- *     *
- *      * Return: 0 Always success
- *       */
+ *  * main - program that prints all possible different combinations of 3 digits.
+ *   * Return: 0
+ *    */
 int main(void)
 {
-		int digit1, digit2, digit3;
+		int c = 0;
+			int f_d;
+				int m_d;
+					int l_d;
 
-			for (digit1 = 0; digit1 < 8; digit1++)
-					{
-								for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-											{
-															for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-																			{
-																								putchar((digit1 % 10) + '0');
-																												putchar((digit2 % 10) + '0');
-																																putchar((digit3 % 10) + '0');
+						while (c <= 999)
+								{
+											f_d = (c / 100 + '0');
+													m_d = (c / 10 % 10 + '0');
+															l_d = (c % 10 + '0');
 
-																																				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-																																										continue;
-																																								putchar(',');
-																																												putchar(' ');
-																																															}
-																	}
-									}
+																	if ((f_d < m_d) && (m_d < l_d))
+																				{
+																								putchar(f_d);
+																											putchar(m_d);
+																														putchar(l_d);
 
-				putchar('\n');
-
-					return (0);
+																																	if (c != 789)
+																																					{
+																																										putchar(',');
+																																														putchar(' ');
+																																																	}
+																																			}
+																			c++;
+																				}
+						putchar('\n');
+						return (0);
 }
