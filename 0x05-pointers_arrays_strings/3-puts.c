bfecd9e1858a2011ba685e7cec166308c043207c
Codeon247 <ugwuokefrank02@gmail.com>
2022-09-19 05:04:18 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: input srting
+ * Return: no return
+ */
+
+void _puts(char *str)
+
+{
+	while (*str)
+
+	_putchar(*str++);
+	_putchar('\n');
+}
